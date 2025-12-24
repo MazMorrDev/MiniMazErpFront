da2be0134f6d99f4c01e1aeccef382fd1d2ccabe
OsvaFernandez <osvanaranjo96@gmail.com>
2025-12-24 16:22:09 -0300

    add product service logic

--- src/app/components/inventory-grid/inventory_grid.component.ts
+++ src/app/components/inventory-grid/inventory_grid.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, Signal, signal, WritableSignal } from '@angular/core';
+import { ProductService } from '../../services/prod.service';
+import { Product } from '../../interfaces/products.interfaces';
 
 @Component({
   selector: 'app-inventory_grid',
@@ -6,10 +8,21 @@ import { Component, OnInit, signal } from '@angular/core';
   styleUrls: ['./inventory_grid.component.css']
 })
 export class InventoryGridComponent implements OnInit {
-  isLoading = signal(false);
-  constructor() { }
+  isLoading : WritableSignal<boolean> = signal(false);
+  prodService = inject(ProductService);
+  products : WritableSignal<Product[]> = signal<Product[]>([]);
 
   ngOnInit(): void {
-    // load objects
+    this.isLoading.set(true);
+    this.prodService.getProducts().subscribe({
+      next: (data) => {
+        this.products.set(data as Product[]);
+        this.isLoading.set(false);
+      },
+      error: (error) => {
+        console.error('Error fetching products:', error);
+        this.isLoading.set(false);
+      }
+    });
   }
 }
