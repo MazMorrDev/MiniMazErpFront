8867c6b50b8ba455c222881b17696b991522288e
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-01-02 20:50:16 -0500

    fix(registerService): use EnvironmentDevelopment insted of Environment

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,17 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Router } from 'express';
-import { Environment } from '../environments/environment';
 import { RegisterRequest } from '../interfaces/register-request';
 import { ApiTokenResponse } from '../interfaces/token-response';
+import { EnvironmentDevelopment } from '../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RegisterService {
   private readonly http = inject(HttpClient);
-  private readonly router = inject(Router);
-  private readonly apiUrl = Environment.apiUrl;
+  private readonly apiUrl = EnvironmentDevelopment.apiUrl;
 
   register(registerRequest: RegisterRequest) {
     const url = `${this.apiUrl}/api/Client/`;
