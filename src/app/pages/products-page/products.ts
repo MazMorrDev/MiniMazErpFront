059ca7858ef4e93fad411e9c361c121f7985ea5d
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-01-02 21:15:48 -0500

    ref: change some components name

--- src/app/pages/products-page/products.ts
+++ src/app/pages/products-page/products.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { InventoryGridComponent } from "../../components/inventory-grid/inventory_grid.component";
+import { InventoryGridComponent } from "../../components/inventory-grid/inventory_grid";
 
 @Component({
   selector: 'app-products',
