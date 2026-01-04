a64d83ab04f0bab6e34eafdfdf4780863b634a92
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-01-03 23:12:06 -0500

    feat: inject the apiUrl and httpClient to movement services

--- src/app/pages/movements/services/sell.service.ts
+++ src/app/pages/movements/services/sell.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { EnvironmentDevelopment } from '../../../environments/environment-development';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SellService {
-  
+  private readonly apiUrl = EnvironmentDevelopment.apiUrl;
+  private readonly http = inject(HttpClient);
 }
